2e8a07e74101ef4b65a720b5537ca46016e99ec7
Farzin Golkhosh <farzin.golkhosh@gmail.com>
2020-08-22 15:09:01 -0700

    [FIX] Pass sensor data to inverse model

--- src/occgridmapping.cpp
+++ src/occgridmapping.cpp
@@ -2,7 +2,8 @@
 #include <math.h>
 #include <iostream>
 
-Mapping::Mapping(Robot *r) : robot__(r) {
+Mapping::Mapping(Robot *r) : robot__(r)
+{
     std::cout << "Initializing the occupancy grid mapping..." << std::endl;
 }
 
@@ -25,8 +26,10 @@ void Mapping::occ_grid_mapping()
     Map *map = robot__->get_map();
 
     double sensor_max_range = (robot__->get_sensor())->get_max_range();
-
+    // get robot pose
     State robot_states = robot__->get_pose();
+    // get sensor data
+    std::vector<double> sensor_data = (robot__->get_sensor())->get_readings(robot_states.x, robot_states.y, nullptr, true);
 
     while (robot_states.time_stamp != -1)
     {
@@ -39,17 +42,18 @@ void Mapping::occ_grid_mapping()
                 // compute centre of mass of each cell
                 double com_x = x * grid_width + (grid_width / 2) - robot_width;
                 double com_y = -(y * grid_height + grid_height / 2) + robot_height;
-                // check if cell is robot perception feild
+                // check if cell is in robot perception feild
                 double r = sqrt(pow(robot_states.x - com_x, 2) + pow(robot_states.y - com_y, 2));
-                // assuming if r is within the sensor range it's inside the perception field since we have 8 sonar sensors and field is almost a circle
+                // assuming if r is within the sensor range it's inside
                 if (r <= robot__->get_sensor()->get_max_range())
                 {
-                    (*map)[y][x] = (*map)[y][x] + (robot__->get_sensor())->inverse_model(robot_states.x, robot_states.y, robot_states.orientation, com_x, com_y);
+                    (*map)[y][x] = (*map)[y][x] + (robot__->get_sensor())->inverse_model(robot_states.x, robot_states.y, robot_states.orientation, com_x, com_y, l_o__, l_free__, l_occ__, sensor_data);
                 }
             }
         }
         // read the next recording
         robot_states = robot__->get_pose();
+        std::vector<double> sensor_data = (robot__->get_sensor())->get_readings(robot_states.x, robot_states.y);
     }
 }
 
